e00862150e20ae06ca7bf5a16cff52a47c3921ff
maxi-badaro <maxibadaro@gmail.com>
2022-01-07 19:05:34 -0300

    warning no solucionado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
 import './App.css';
 import NavBar from './components/NavBar';
+import Componente from "./components/Componente";
 
 function App() {
   return (
     <>
     <NavBar />
+    <section>
+      <Componente msg="hola, me pasaron por una prop"/>
+    </section>
     </>
   );
 }
